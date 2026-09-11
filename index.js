b42d9f818ae562b74d03d2e8f02fcebeae09c445
yfydu65uv57-bit <yfydu65uv57@gmail.com>
2026-09-11 13:23:09 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '201213936427', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
   // Owner 1
     { name: "VA", lid: "247579682029763@lid", jid: "972569311531@s.whatsapp.net" },
   // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "201144480436@s.whatsapp.net" },
+    { name: "emam", lid: "221307316789354@lid", jid: "201213936427@s.whatsapp.net" },
   // Owner 3
     { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
   // Owner 4 
