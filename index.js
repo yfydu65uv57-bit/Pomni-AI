4fd9dbdc71f24b3af178f7ce33970bb1c4f3deea
yfydu65uv57-bit <yfydu65uv57@gmail.com>
2026-09-11 13:13:35 +0300

    Update phone number and owner JIDs in index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '201213936427', // Bot number
   prefix: [".", "/", "!"],
   fromMe: false, 
   owners: [
