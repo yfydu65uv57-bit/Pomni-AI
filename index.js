e8c89ec9dbcf6f1df197f9be73b30104df305d38
yfydu65uv57-bit <yfydu65uv57@gmail.com>
2026-09-11 16:11:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const client = new Client({
   // Owner 1
     { name: "VA", lid: "247579682029763@lid", jid: "972569311531@s.whatsapp.net" },
   // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "201144480436@s.whatsapp.net" },
+    { name: "emam", lid: "221307316789354@lid", jid: "201213936427@s.whatsapp.net" },
   // Owner 3
     { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
   // Owner 4 
