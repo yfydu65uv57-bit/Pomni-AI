77f0ced486adf79faa6825e09436b9b0401d5888
yfydu65uv57-bit <yfydu65uv57@gmail.com>
2026-09-13 08:56:55 +0300

    Update yt-button.js

--- plugins/download/yt-button.js
+++ plugins/download/yt-button.js
@@ -8,13 +8,13 @@ const handler = async (m, { conn, text }) => {
     await conn.sendButton(m.chat, {
         imageUrl: image,
         bodyText: `${title} ╎ ${time}`,
-        footerText: "🕸️ 𝐃𝐨𝐰𝐧𝐥𝐨𝐚𝐝 ~ 𝐘𝐨𝐮𝐓𝐮𝐛𝐞 🕸️",
+        footerText: "🕸️ ⟬𝑻𝑬𝑻𝑶(<  >)𝑯𝒂𝑲𝒆𝑴⟭ 🕸️",
         buttons: [
             { name: "quick_reply", params: { display_text: "🎼 ╎ تـحـمـيـل صـوت", id: `.يوت_اغنيه ${url}` } },
             { name: "quick_reply", params: { display_text: "🎬 ╎ تـحـمـيـل فـيـديـو", id: `.يوتيوب ${url}` } }
         ],
         mentions: [m.sender],
-        newsletter: { name: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", jid: "120363225356834044@newsletter" },
+        newsletter: { name: "⟬𝑻𝑬𝑻𝑶(<  >)𝑯𝒂𝑲𝒆𝑴⟭🕷️", jid: "120363225356834044@newsletter" },
         interactiveConfig: { buttons_limits: 10, list_title: "Pomni 🎀", button_title: "Pomni 🎀", canonical_url: url }
     }, m);
 };
